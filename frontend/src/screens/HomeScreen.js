f324c777cbad8d4c124fd10557f95bf1f6487321
aba450 <54678566+aba450@users.noreply.github.com>
2020-11-28 18:02:52 -0500

    added cart functionality

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,33 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import { Row, Col} from 'react-bootstrap'
-// import products from '../products'
-import Product from '../components/Product'
-import axios from 'axios'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listProducts } from '../actions/productActions';
+
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
 
-    const [products, setProducts] = useState([])
+  const { loading, error, products } = productList;
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            setProducts(data)
-        } 
-        fetchProducts()
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-    }, [])
-    return (
-        <>
-        <h1> Latest Products</h1>
+  return (
+    <>
+      <h1> Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'> {error} </Message>
+      ) : (
         <Row>
-            {products.map(product => (
-                <Col key= {product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-                </Col>
-            )
-                )}
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
         </Row>
-        </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
