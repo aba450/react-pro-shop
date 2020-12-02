4299181a17d230d770cec5b3c4cfb02ddc9c83ff
aba450 <54678566+aba450@users.noreply.github.com>
2020-12-02 15:15:08 -0500

    Adding controller

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,29 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleWare.js';
 dotenv.config();
 
 connectDB();
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is running....');
 });
 
 app.use('/api/products', productRoutes);
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/users', userRoutes);
+// app.get('/api/products', (req, res) => {
+//   res.json(products);
+// });
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+// app.get('/api/products/:id', (req, res) => {
+//   const product = products.find((p) => p._id === req.params.id);
+//   res.json(product);
+// });
 app.use(notFound);
 app.use(errorHandler);
 
